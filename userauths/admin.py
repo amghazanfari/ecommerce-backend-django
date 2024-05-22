263eee154062e2f9e00d53336d8e227bd5a43fc9
amghazanfari <a.m.ghazanfari76@gmail.com>
2024-05-23 00:33:16 +0330

    add custom user

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from userauths.models import Profile, User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "phone", "full_name"]
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["full_name", "gender", "country"]
+    search_fields = ["full_name"]
+    list_filter = ["date"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
