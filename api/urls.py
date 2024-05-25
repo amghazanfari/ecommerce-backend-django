e15b7b12ce9c6e357124eb1db1d68c5d1067af70
amghazanfari <a.m.ghazanfari76@gmail.com>
2024-05-25 13:07:16 +0330

    add product urls and views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from userauths import views as userauths_views
+from store import views as store_views
 
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -9,5 +10,9 @@ urlpatterns = [
     path("user/token/refresh/   ", TokenRefreshView.as_view(), name="TokenRefreshView"),
     path("user/register/", userauths_views.RegisterView.as_view(), name="RegisterView"),
     path("user/password-reset/<email>/", userauths_views.PasswordResetEmailVerify.as_view(), name="PasswordResetEmailVerify"),
-    path("user/password-change/", userauths_views.PasswordChangeView.as_view(), name="PasswordChangeView")
+    path("user/password-change/", userauths_views.PasswordChangeView.as_view(), name="PasswordChangeView"),
+
+    path("category/", store_views.CategoryListAPIView.as_view(), name="CategoryListAPIView"),
+    path("product/", store_views.ProductListAPIView.as_view(), name="ProductListAPIView"),
+    path("product/<slug>/", store_views.ProductDetailAPIView.as_view(), name="ProductDetailAPIView"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from store.models import Category, Product, Gallery, Specification, Size,\
+      Color, Cart, CartOrder, CartOrderItem, ProductFaq, Review, WhishList,\
+          Notification, Coupon
+from store.serializers import ProductSerializer, CategorySerializer
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (AllowAny,)
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (AllowAny,)
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (AllowAny,)
+
+    def get_object(self):
+        slug = self.kwargs['slug']
+        return Product.objects.get(slug=slug)
